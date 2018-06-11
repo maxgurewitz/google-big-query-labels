998761614fad95ec07be54727e3ed17cc64bffe1
Maxwell Gurewitz <maxthegeek1@gmail.com>
2018-06-11 15:23:37 -0700

    wip

--- src/index.js
+++ src/index.js
@@ -1,27 +1,58 @@
-const UuidEncoder = require('uuid-encoder');
+const bigInt = require('big-integer');
 
 const prefix = 'l';
-
-let lowerCase = '';
-
-for (let i = 0; i < 26; i++) {
-  lowerCase = lowerCase.concat(String.fromCharCode(97 + i));
-}
-
-const characters = '_-0123456789' + lowerCase;
-
-const encoder = new UuidEncoder(characters);
+const characters = '_-0123456789abcdefghijklmnopqrstuvwxyz';
 
 module.exports = {prefix, encode, decode, characters};
 
-function encode(str) {
+function encode(uuid, base=16) {
+  const cleanUuid = uuid.replace(/-/g, '');
+
+  let iUuid;
   try {
-    return prefix + encoder.encode(str);
+    console.log('loc6', base, cleanUuid);
+    iUuid = bigInt(cleanUuid, base);
   } catch (e) {
-    throw new TypeError('Encode must be called with a valid uuid.');
+    throw new TypeError(`${uuid} is not a valid base ${base} string.`);
   }
+  console.log('loc5', iUuid.toString(10));
+
+  let str = '';
+
+  do {
+    str = characters.substr(iUuid.mod(characters.length).valueOf(), 1) + str;
+    iUuid = iUuid.divide(characters.length);
+  } while (iUuid.greater(0));
+
+  return prefix + str;
 }
 
-function decode(str) {
-  return encoder.decode(str.slice(1, str.length));
+// FIXME not preserving case
+function decode(encoded, base=16, hyphenated=true) {
+  let iUuid = bigInt(0);
+
+  const str = encoded.slice(1, encoded.length);
+  const len = str.length;
+
+  for (let pos = 0; pos < len; pos += 1) {
+    const ch = str.substr(pos, 1);
+    const encPos = characters.indexOf(ch);
+
+    if (encPos < 0) {
+      throw new Error('Invalid encoded data');
+    }
+
+    iUuid = iUuid.add(encPos);
+
+    if (pos < len - 1) {
+      iUuid = iUuid.multiply(characters.length);
+    }
+
+  }
+
+  const uuid = iUuid.toString(base).padStart(32, '0');
+
+  return hyphenated ?
+    `${uuid.substr(0, 8)}-${uuid.substr(8, 4)}-${uuid.substr(12, 4)}-${uuid.substr(16, 4)}-${uuid.substr(20)}` :
+    uuid;
 }
--- test/index.spec.js
+++ test/index.spec.js
@@ -6,7 +6,7 @@ describe('when provided a string to encode', () => {
 
   it('throws an exception when not provided a uuid', () => {
     const nonUuid = 'non uuid string';
-    expect(() => encode(nonUuid)).to.throw('Encode must be called with a valid uuid.');
+    expect(() => encode(nonUuid)).to.throw();
   });
 
   it('encoded string only contains approved characters', () => {
@@ -27,10 +27,21 @@ describe('when provided a string to encode', () => {
 
 describe('when provided a string to decode', () => {
 
-  it('returns original value', () => {
+  it('returns original value when provided a standard base16 encoded uuid', () => {
     const original = uuidv1();
     const encoded = encode(original);
     const decoded = decode(encoded);
+
+      // -384adb06-dc21-1e88-ea46-d46a7c19ffd
+      // +0384adb0-6dc2-11e8-8ea4-6d46a7c19ffd
     expect(decoded).to.equal(original);
+  });
+
+  describe('returns original when provided a base64 encoded string', () => {
+    // const base64Encoded = 'IQFQZGazYDgTQbjLpte43jNLbw';
+    const base64Encoded = 'A';
+    const encoded = encode(base64Encoded, 64);
+    const decoded = decode(encoded, 64, false);
+    expect(decoded).to.equal(base64Encoded);
   });
 });
