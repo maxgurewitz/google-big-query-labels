f3dfce35c63de90dfb5791fd589605804d1a424d
Maxwell Gurewitz <maxthegeek1@gmail.com>
2018-06-11 11:34:39 -0700

    list of approved characters

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 const uuidEncoder = require('uuid-encoder');
+
 const prefix = 'l';
 
-module.exports = {prefix, encode, decode};
+let lowerCase = '';
+
+for (let i = 0; i < 26; i++) {
+  lowerCase = lowerCase.concat(String.fromCharCode(97 + i));
+}
+
+const characters = '_-0123456789' + lowerCase;
+
+module.exports = {prefix, encode, decode, characters};
 
 function encode() {
 }
